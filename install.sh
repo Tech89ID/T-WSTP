02480c74b174e392814003d4b03877bbb1c011a8
Tech89ID <97566742+Tech89ID@users.noreply.github.com>
2022-01-24 15:00:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 pkg update; pkg upgrade -y
 pkg install php screen openssh wget curl -y
+
 chmod +x start
+chmod +x stop
 chmod +x bot-telegram
+
 echo y|ssh-keygen -t ed25519 -f ~/.ssh/t-wstp -N ""
 ./start
