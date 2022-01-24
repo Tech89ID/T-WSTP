bb72a379cd88f5c8eaf8883ecbae0eac2f1b90fc
Tech89ID <97566742+Tech89ID@users.noreply.github.com>
2022-01-24 13:52:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkg update && pkg upgrade -y
-pkg install php screen openssh wget -y
+pkg install php screen openssh wget curl -y
 chmod +x start
 echo y|ssh-keygen -t ed25519 -f ~/.ssh/t-wstp -N ""
 ./start
